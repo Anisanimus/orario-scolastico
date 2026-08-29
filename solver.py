6a71ff6f69570d274ff8cfd9eefb7137aaf2aa12
School Scheduler User <user@school.local>
2026-08-29 10:33:33 +0200

    fix: vincolo esclusivo palestra - solo ed esclusivamente le classi terze in gruppo parallelo possono condividere la palestra

--- solver.py
+++ solver.py
@@ -668,10 +668,6 @@ class TimetableSolver:
 
             for d in range(num_days):
                 for h in range(daily_hours[d]):
-                    # In qualunque ora (d, h), lo spazio speciale/palestra può essere occupato da:
-                    # - 1 gruppo parallelo autorizzato (es. 3ªA+3ªB), OPPURE
-                    # - 1 singola classe ordinaria (stand-alone)
-                    # MAI due classi ordinarie insieme, e MAI una classe ordinaria insieme a un gruppo parallelo!
                     pg_active_indicators = []
                     for grp, g_a_ids in pg_in_room:
                         p_vars_dict = self.parallel_group_slot_vars.get(grp.id)
@@ -680,9 +676,22 @@ class TimetableSolver:
                         else:
                             pg_active_indicators.append(self.x[g_a_ids[0], d, h])
                     
-                    # Ciascun gruppo parallelo vale 1 'occupazione di palestra' e ogni classe stand-alone vale 1 'occupazione'
-                    # Il totale delle occupazioni indipendenti non può superare 1!
-                    m.Add(sum(self.x[a_id, d, h] for a_id in stand_alone_room_assigns) + sum(pg_active_indicators) <= 1)
+                    if len(rooms_in_grp) == 1 and pg_in_room:
+                        # Spazio speciale unico (es. Palestra Unica con gruppi paralleli autorizzati):
+                        # Ciascun gruppo parallelo autorizzato conta come 1 occupazione e ciascuna classe singola conta come 1 occupazione.
+                        # Il totale delle occupazioni indipendenti non può superare 1 (nessuna sovrapposizione casuale!).
+                        m.Add(sum(self.x[a_id, d, h] for a_id in stand_alone_room_assigns) + sum(pg_active_indicators) <= 1)
+                    else:
+                        # Laboratori con spazi multipli (es. Lab Arte 1 + Lab Arte 2)
+                        slot_terms = [self.x[a_id, d, h] for a_id in stand_alone_room_assigns]
+                        for grp, g_a_ids in pg_in_room:
+                            p_vars_dict = self.parallel_group_slot_vars.get(grp.id)
+                            if p_vars_dict and (d, h) in p_vars_dict:
+                                p_v = p_vars_dict[d, h]
+                                slot_terms.append(sum(self.x[aid, d, h] for aid in g_a_ids) - (len(g_a_ids) - 1) * p_v)
+                            else:
+                                slot_terms.append(self.x[g_a_ids[0], d, h])
+                        m.Add(sum(slot_terms) <= total_cap)
 
         # 9. VINCOLO DIDATTICO RIGIDO: Max ore al giorno per materia in una classe
         for a in prob.assignments:
