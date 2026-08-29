5e1f720f9f786d703470fcf084fb2cff2de197df
School Scheduler User <user@school.local>
2026-08-29 20:53:42 +0200

    feat: aggiunto Passo 7 con visualizzazione griglia orario classi e docenti

--- server.py
+++ server.py
@@ -78,6 +78,51 @@ class AppServer(http.server.SimpleHTTPRequestHandler):
             self.wfile.write(json.dumps(data).encode("utf-8"))
             return
 
+        if parsed.path == "/api/timetable":
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            if not current_result or not current_result.class_schedules:
+                self.wfile.write(json.dumps({"has_schedule": False, "days": DAYS_OF_WEEK[:current_problem.config.num_days]}).encode("utf-8"))
+                return
+                
+            num_days = current_problem.config.num_days
+            days = DAYS_OF_WEEK[:num_days]
+            max_daily_h = max(current_problem.config.daily_hours[:num_days])
+            
+            # Format classes timetables
+            classes_tables = {}
+            for c_id, c in current_problem.classes.items():
+                grid = [["---" for _ in range(num_days)] for _ in range(max_daily_h)]
+                c_sched = current_result.class_schedules.get(c_id, {})
+                for (d, h), item in c_sched.items():
+                    if d < num_days and h < max_daily_h:
+                        s_name = current_problem.subjects[item.subject_id].name if item.subject_id in current_problem.subjects else item.subject_id
+                        t_name = current_problem.teachers[item.teacher_id].name if item.teacher_id in current_problem.teachers else item.teacher_id
+                        grid[h][d] = f"<b>{s_name}</b><br><span style='font-size:11px; color:#475569;'>{t_name}</span>"
+                classes_tables[c_id] = {"name": c.name, "grid": grid}
+
+            # Format teachers timetables
+            teachers_tables = {}
+            for t_id, t in current_problem.teachers.items():
+                grid = [["---" for _ in range(num_days)] for _ in range(max_daily_h)]
+                t_sched = current_result.teacher_schedules.get(t_id, {})
+                for (d, h), item in t_sched.items():
+                    if d < num_days and h < max_daily_h:
+                        c_name = current_problem.classes[item.class_id].name if item.class_id in current_problem.classes else item.class_id
+                        s_name = current_problem.subjects[item.subject_id].name if item.subject_id in current_problem.subjects else item.subject_id
+                        grid[h][d] = f"<b>{c_name}</b><br><span style='font-size:11px; color:#475569;'>{s_name}</span>"
+                teachers_tables[t_id] = {"name": t.name, "grid": grid}
+
+            self.wfile.write(json.dumps({
+                "has_schedule": True,
+                "days": days,
+                "max_hours": max_daily_h,
+                "classes": classes_tables,
+                "teachers": teachers_tables
+            }).encode("utf-8"))
+            return
+
         return super().do_GET()
 
     def do_POST(self):
